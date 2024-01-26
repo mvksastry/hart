47f4695633b130f22718eb3b231ecd7514a94549
mvksastry <mvksastry@gmail.com>
2024-01-26 15:30:42 +0530

    licensing blogs were changed

--- resources/views/licensing/perpetual.blade.php
+++ resources/views/licensing/perpetual.blade.php
@@ -11,34 +11,91 @@
             </div>
           </div>
         </section>
-        <section class="module">
+        
+        <section class="module-small">
           <div class="container">
             <div class="row">
-              <div class="col-sm-6">
-                <h5 class="font-alt">We’re a digital creative agency</h5><br/>
-                <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
-                <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
+              <div class="col-sm-4 col-md-3 sidebar">
+                <div class="widget">
+                </div>
+                <div class="widget">
+                  <h5 class="widget-title font-alt">Contents</h5>
+                  <ul class="icon-list">
+                    <li><a href="#">Current State</a></li>
+                    <li><a href="#">Pros and Cons</a></li>
+                    <li><a href="#">The Decision</a></li>
+                    <li><a href="#">The Choice</a></li>
+                  </ul>
+                </div>
               </div>
-              <div class="col-sm-6">
-                <h6 class="font-alt"><span class="icon-tools-2"></span> Development
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="60" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
+              <div class="col-sm-8 col-sm-offset-1">
+                <div class="post">
+                  <div class="post-thumbnail">
+                  <!-- <img src="assets/images/post-4.jpg" alt="Blog Featured Image"/> -->
+                  </div>
+                  <div class="post-header font-alt">
+                    <h1 class="post-title">Perpetual License: Myth or Reality?</h1>
+                    <div class="post-meta">By&nbsp;<a href="#">Krishna Teja</a>| 23 November | 
+                    </div>
+                  </div>
+                  <div class="post-entry">
+                    <p><b><h4>Current State</h4></b>Since when computers and software began to evovle, a one-time
+                    purchase of a PC used to come with software that it works perpetual or eternal.
+                    However, nothing in the universe works perpetual except planet rotaion! For everything
+                    else, there is a limit, so does software. Except a few software categories such as computers
+                    that operate equipment, everthing else has to keep pace with the changing environment.
+                    </p>
+                    <p>
+                    In order to keep pace with the changing environment, comfort and threats increase with time and so does the need to update.
+                    Any reluctance to update or resistance to change may result in damages that might not be easy
+                    to recover, both interms of time and effort. Moreover, such resistance to change may result in
+                    heavy loss of valuable data which can threaten the very existence of entities. 
+                    </p>
+                    <p><b><h4>Definition</h4></b>
+                    Perpetual licensing is when a software vendor sells a license to a user for a one-time fee. 
+                    </p>
+                    <p>Usually, annual maintenance contracts are needed to implement changes and dedicated personnel to keep track changes
+                    in the code implemented. Often newer versions or branches are not covered and everything needed is fixed for life.
+                    As long as the original installation is running nothing to change and it will continue to give the service.</p>
+                    <blockquote>
+                      <p>Excellence is never an accident. It is always the result of high intention, 
+                      sincere effort, and intelligent execution; it represents the wise choice 
+                      of many alternatives - choice, not chance, determines your destiny.
+                      --Aristotle</p>
+                    </blockquote>
+                    <p><b><h5>Pros</h5></b> As with everything there are pros and cons. Let's examine them.</p>
+                    <ul>
+                      <li>It involves a one time fee and user can have as long as it performs.</li>
+                      <li>If your entity is not going to grow in employees or work flow dramatically, this is the best choice</li>
+                      <li>For a given hardware, with annual maintenance contract, one can get going.</li>
+                    </ul>
+                    
+                    <p><b><h5>Cons</h5></b></p>
+                    <ul>
+                      <li>It can go out-dated much sooner than anticipated.</li>
+                      <li>Growth of organization can affect the outcome and performance of the product
+                      </li>
+                      <li>No matter with any amount of AMC, it is insufficient to keep pace with change.</li>
+                    </ul>
+                    
+                    <p>The European languages are members of the same family. Their separate existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary. The languages only differ in their grammar, their pronunciation and their most common words.</p>
+
+
+                    <p><b><h4>Decision</h4></b>
+                      An office that is growing with number or people or work-flows is not a static entity, no matter
+                      how simple the work is. Perpetual license for an office is not a good decision unless no intention of growing. 
+                    </p>
+                    
+                    <p><b><h4>Choice</h4></b>
+                      Investing high amount of money upfront for product that goes outdated sooner is never a good choice.
+                      It is better to leave pepetual license and go for subscription based license for growing organizations.
+                       
+                    </p>
+ 
+
+                  </div>
                 </div>
-                <h6 class="font-alt"><span class="icon-strategy"></span> Branding
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="80" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
-                </div>
-                <h6 class="font-alt"><span class="icon-target"></span> Marketing
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="50" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
-                </div>
-                <h6 class="font-alt"><span class="icon-camera"></span> Photography
-                </h6>
-                <div class="progress">
-                  <div class="progress-bar pb-dark" aria-valuenow="90" role="progressbar" aria-valuemin="0" aria-valuemax="100"><span class="font-alt"></span></div>
+                <div class="comments">
                 </div>
               </div>
             </div>
