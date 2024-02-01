4dc08e62b51d88c7326ebe34dfbcb4e8c327f849
mvksastry <mvksastry@gmail.com>
2024-02-01 07:43:19 +0530

    kanban board related changes made, js to be done for this

--- resources/views/layouts/kanban/kanbanBoard.blade.php
+++ resources/views/layouts/kanban/kanbanBoard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
 	<!-- Content Wrapper. Contains page content -->
-	<div class="content-wrapper kanban">
+	<div class="content-wrapper">
     <section class="content-header">
       <div class="container-fluid">
         <div class="row">
@@ -15,177 +15,195 @@
             </ol>
           </div>
         </div>
+        <!-- / end header -->
+        <!-- begin kanban input fields -->
+        <div class="row">
+          <div class="col-sm-12">        
+            <div class="controls p-3">
+              <form class="form-inline">
+                <label class="ml-4" for="titleInput">Title:</label>
+                <input class="form-control form-control-sm ml-2" type="text" name="title" id="titleInput" autocomplete="off">
+                <label class="ml-2" for="descriptionInput">Description:</label>
+                <input class="form-control form-control-sm ml-2" type="text" name="description" id="descriptionInput" autocomplete="off">
+                <button class="btn btn-dark ml-2" id="add">Add</button>
+                <button class="btn btn-danger mx-2" id="deleteAll">Delete All</button>
+              </form>
+            </div>
+          </div>
+        </div> 
+        <!-- / begin kanban input fields -->        
       </div>
     </section>
 
 		<!-- Main content -->
 		<section class="content pb-3">
-			<div class="container-fluid h-100">
-				
-        <div class="card card-row card-secondary">
-          <div class="card-header">
-            <h3 class="card-title">
-              Backlog
-            </h3>
-          </div>
-          <div class="card-body">
-            <div class="card card-info card-outline">
+			<div class="container-fluid">
+        <div class="row">
+          <div class="col-sm-3">
+            <div class="card card-row card-secondary">
               <div class="card-header">
-                <h5 class="card-title">Create Labels</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#3</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
+                <h3 class="card-title">
+                  Backlog
+                </h3>
               </div>
               <div class="card-body">
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox1" disabled>
-                  <label for="customCheckbox1" class="custom-control-label">Bug</label>
+                <div class="card card-info card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Labels</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#3</a>
+                      <a href="#" class="btn btn-tool"><i class="fas fa-pen"></i></a>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox1" disabled>
+                      <label for="customCheckbox1" class="custom-control-label">Bug</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox2" disabled>
+                      <label for="customCheckbox2" class="custom-control-label">Feature</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled>
+                      <label for="customCheckbox3" class="custom-control-label">Enhancement</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox4" disabled>
+                      <label for="customCheckbox4" class="custom-control-label">Documentation</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox5" disabled>
+                      <label for="customCheckbox5" class="custom-control-label">Examples</label>
+                    </div>
+                  </div>
                 </div>
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox2" disabled>
-                  <label for="customCheckbox2" class="custom-control-label">Feature</label>
+                <div class="card card-primary card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Issues</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#4</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox1_1" disabled>
+                      <label for="customCheckbox1_1" class="custom-control-label">Bug Report</label>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                      <input class="custom-control-input" type="checkbox" id="customCheckbox1_2" disabled>
+                      <label for="customCheckbox1_2" class="custom-control-label">Feature Request</label>
+                    </div>
+                  </div>
                 </div>
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox3" disabled>
-                  <label for="customCheckbox3" class="custom-control-label">Enhancement</label>
+                <div class="card card-primary card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">PR</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#6</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
                 </div>
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox4" disabled>
-                  <label for="customCheckbox4" class="custom-control-label">Documentation</label>
-                </div>
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox5" disabled>
-                  <label for="customCheckbox5" class="custom-control-label">Examples</label>
-                </div>
-              </div>
-            </div>
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Create Issue template</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#4</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body">
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox1_1" disabled>
-                  <label for="customCheckbox1_1" class="custom-control-label">Bug Report</label>
-                </div>
-                <div class="custom-control custom-checkbox">
-                  <input class="custom-control-input" type="checkbox" id="customCheckbox1_2" disabled>
-                  <label for="customCheckbox1_2" class="custom-control-label">Feature Request</label>
-                </div>
-              </div>
-            </div>
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Create PR template</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#6</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div class="card card-light card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Create Actions</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#7</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa.
-                  Cum sociis natoque penatibus et magnis dis parturient montes,
-                  nascetur ridiculus mus.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-        <div class="card card-row card-primary">
-          <div class="card-header">
-            <h3 class="card-title">
-              To Do
-            </h3>
-          </div>
-          <div class="card-body">
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Create first milestone</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#5</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
+                <div class="card card-primary card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Actions</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#7</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <p>
+                      Some text here.
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div class="card card-row card-default">
-          <div class="card-header bg-info">
-            <h3 class="card-title">
-              In Progress
-            </h3>
-          </div>
-          <div class="card-body">
-            <div class="card card-light card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Update Readme</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#2</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
-              </div>
-              <div class="card-body">
-                <p>
-                  Lorem ipsum dolor sit amet, consectetuer adipiscing elit.
-                  Aenean commodo ligula eget dolor. Aenean massa.
-                  Cum sociis natoque penatibus et magnis dis parturient montes,
-                  nascetur ridiculus mus.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="card card-row card-success">
-          <div class="card-header">
-            <h3 class="card-title">
-              Done
-            </h3>
-          </div>
-          <div class="card-body">
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="card-title">Create repo</h5>
-                <div class="card-tools">
-                  <a href="#" class="btn btn-tool btn-link">#1</a>
-                  <a href="#" class="btn btn-tool">
-                    <i class="fas fa-pen"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> 
 
-        
+          <div class="col-sm-3">
+            <div class="card card-row card-primary">
+              <div class="card-header">
+                <h3 class="card-title">
+                  To Do
+                </h3>
+              </div>
+              <div class="card-body">
+                <div class="card card-primary card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Milestone</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#5</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">              
+            <div class="card card-row card-default">
+              <div class="card-header bg-info">
+                <h3 class="card-title">
+                  In Progress
+                </h3>
+              </div>
+              <div class="card-body">
+                <div class="card card-light card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Updates</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#2</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <p>
+                      Some text here
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-sm-3">              
+            <div class="card card-row card-success">
+              <div class="card-header">
+                <h3 class="card-title">
+                  Done
+                </h3>
+              </div>
+              <div class="card-body">
+                <div class="card card-primary card-outline">
+                  <div class="card-header">
+                    <h5 class="card-title">Reports</h5>
+                    <div class="card-tools">
+                      <a href="#" class="btn btn-tool btn-link">#1</a>
+                      <a href="#" class="btn btn-tool">
+                        <i class="fas fa-pen"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div> 
+          </div>
+        </div>              
       </div>
     </section>
 	</div>	
