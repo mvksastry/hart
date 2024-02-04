ba65244da6ed58b7877f64a5e7f81ccb258906d5
mvksastry <mvksastry@gmail.com>
2024-02-04 16:45:28 +0530

    cal changes

--- resources/views/calendar/myCalendar.blade.php
+++ resources/views/calendar/myCalendar.blade.php
@@ -53,15 +53,7 @@
                   <h3 class="card-title">Create Event</h3>
                 </div>
                 <div class="card-body">
-                  <div class="btn-group" style="width: 100%; margin-bottom: 10px;">
-                    <ul class="fc-color-picker" id="color-chooser">
-                      <li><a class="text-primary" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-warning" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-success" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-danger" href="#"><i class="fas fa-square"></i></a></li>
-                      <li><a class="text-muted" href="#"><i class="fas fa-square"></i></a></li>
-                    </ul>
-                  </div>
+
                   <!-- /btn-group -->
                   <div class="form-group">
                     <form id="calEventForm" role="form" method="POST" data-action="{{ route('calendar.store') }}">
