94fc0916bd5b9c123c98fde13dd53791bb8af54a
mvksastry <mvksastry@gmail.com>
2024-02-09 07:24:23 +0530

    edit corrections

--- resources/views/projects/indexProjectTasks.blade.php
+++ resources/views/projects/indexProjectTasks.blade.php
@@ -39,7 +39,7 @@
 						  <div class="card-header">
 							<h3 class="card-title">
 							  <i class="fas fa-chart-pie mr-1"></i>
-							  Events
+							  Tasks
 							</h3>
 							<div class="card-tools">
 							  <ul class="nav nav-pills ml-auto">
